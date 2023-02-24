a45297591af36ea737dae0d0d52048a82001bb12
Anderson-Leandro <andersonmendesleandro@gmail.com>
2023-02-24 20:42:42 -0300

    feat: add task 3 funcionalities

--- songs/views.py
+++ songs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView, Response, status
+from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Song
@@ -8,29 +9,42 @@ from .serializers import SongSerializer
 from albums.models import Album
 
 
-class SongView(APIView, PageNumberPagination):
+# class SongView(APIView, PageNumberPagination):
+#     authentication_classes = [JWTAuthentication]
+#     permission_classes = [IsAuthenticatedOrReadOnly]
+
+#     def get(self, request, pk):
+#         """
+#         Obtençao de musicas
+#         """
+#         songs = Song.objects.filter(album_id=pk)
+
+#         result_page = self.paginate_queryset(songs, request)
+#         serializer = SongSerializer(result_page, many=True)
+
+#         return self.get_paginated_response(serializer.data)
+
+#     def post(self, request, pk):
+#         """
+#         Criaçao de musica
+#         """
+#         album = get_object_or_404(Album, pk=pk)
+
+#         serializer = SongSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save(album=album)
+
+#         return Response(serializer.data, status.HTTP_201_CREATED)
+
+
+class SongView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get(self, request, pk):
-        """
-        Obtençao de musicas
-        """
-        songs = Song.objects.filter(album_id=pk)
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
 
-        result_page = self.paginate_queryset(songs, request)
-        serializer = SongSerializer(result_page, many=True)
+    def perform_create(self, serializer):
+        album_id = self.request.parser_context["kwargs"]["pk"]
 
-        return self.get_paginated_response(serializer.data)
-
-    def post(self, request, pk):
-        """
-        Criaçao de musica
-        """
-        album = get_object_or_404(Album, pk=pk)
-
-        serializer = SongSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(album=album)
-
-        return Response(serializer.data, status.HTTP_201_CREATED)
+        serializer.save(album_id=album_id)
